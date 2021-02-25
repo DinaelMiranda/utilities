10727186e652edb3d9cdb197d18aadd76ad86bcd
dinael <tadsdinael@gmail.com>
2021-02-25 15:19:43 -0300

    Rorg

---
+++ functions.js
@@ -0,0 +1,15 @@
+let produto = { nome: "Sapato", preco: 150 };
+
+formulaImpostoA = function (preco) {
+  return preco * 0.1;
+};
+formulaImpostoB = function (preco) {
+  return preco * 0.2;
+};
+
+calculaPrecoFinal = function (produto, formulaImposto) {
+  return produto["preco"] + formulaImposto(produto["preco"]);
+};
+
+calculaPrecoFinal(produto.preco, formulaImpostoA);
+calculaPrecoFinal(produto.preco, formulaImpostoB);
